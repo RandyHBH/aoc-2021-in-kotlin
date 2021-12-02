bbb66f083d42ac3030ca46de431edeebf0a3653b
Randy Hector <randy.huergo@numberfour.eu>
2021-12-02 22:34:17 +0100

    Add aoc day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // A + B + C = B + C + D
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(size = 3) { it.sum() }
+            .windowed(2).count { (a, b) -> a < b }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsInt("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(part1(input))
     println(part2(input))
 }
